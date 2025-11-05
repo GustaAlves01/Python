03d8803b9bf3737a57c6b1e150eaad5c262e38d9
Gustavo Alves <gustavoalima14057@gmail.com>
2025-11-05 18:04:06 -0300

    primeira versao

---
+++ app.py
@@ -0,0 +1,7 @@
+import os, pyautogui as py
+from time import sleep
+py.hotkey("win","r")
+py.write("msedge https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+py.press("enter")
+sleep(4)
+print(py.position())
