04d458348b72ce2775a0766b6ae2d3b8b17541ad
Gustavo Alves <gustavoalima14057@gmail.com>
2025-11-05 18:37:16 -0300

    testes de with open

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os, pyautogui as py
 from time import sleep
-py.hotkey("win","r")
+#posição x y
+with open("save.txt","r") as position:
+    cord=position.read().strip()
+    x,y = map(int, cord.split(","))
+    sleep(5)
+    py.click(x,y)
+
+'''py.hotkey("win","r")
 py.write("msedge https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
 py.press("enter")
-sleep(4)
+sleep(4)'''
--- position.py
+++ position.py
@@ -1,5 +1,7 @@
 import pyautogui as py, time
 time.sleep(5)
+
+#posição de log-in
 x,y=py.position()
 with open("save.txt","w") as save:
-    save.write(f"log-in: {x}, {y}")
+    save.write(f"{x}, {y}")
