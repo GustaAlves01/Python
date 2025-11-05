70938c7eef0ed4ebab40d2d4991bf9dcbf7f8e27
Gustavo Alves <gustavoalima14057@gmail.com>
2025-11-05 19:07:04 -0300

    separação por funções

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os, pyautogui as py
 from time import sleep
 #posição x y
-with open("save.txt","r") as position:
-    cord=position.read().strip()
-    x,y = map(int, cord.split(","))
-    sleep(5)
-    py.click(x,y)
 
-'''py.hotkey("win","r")
-py.write("msedge https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
-py.press("enter")
-sleep(4)'''
+with open ("ra.txt","r") as ra:
+    ra = ra.read().strip
+
+
+def Entrar():
+    py.hotkey("win","r")
+    py.write("msedge -inprivate https://saladofuturo.educacao.sp.gov.br/escolha-de-perfil")
+    py.press("enter")
+
+def Logar():
+    with open("save.txt","r") as position:
+        cord=position.read().strip()
+    x,y = map(int, cord.split(","))
+    sleep(4)
+
+Logar()
+sleep(3)
+Entrar()
