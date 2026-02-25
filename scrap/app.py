d84a77830fcfb29aa0bd78d8856e10a240a548d8
Gustavo <gustavoalima14057@gmail.com>
2026-02-25 17:15:22 -0300

    scrap de livros

--- scrap/app.py
+++ scrap/app.py
@@ -1,11 +1,20 @@
 import requests
 from bs4 import BeautifulSoup as bs
-from time import sleep
 
-res = requests.get("https://g1.globo.com/")
-content = bs(res.content, "html.parser")
-noticia = content.find("div", class_="feed-post-body")
-titulo = noticia.find("p", elementtiming="text-ssr").text
-url = noticia.find("a", class_="feed-post-link gui-color-primary gui-color-hover").text
-print(f"titulo: {titulo} aaaa")
-print(url)
+
+site = "https://books.toscrape.com/catalogue/page-1.html"
+res = requests.get(site, timeout=10)
+if (res.status_code == 200):
+	content = bs(res.content, "html.parser")
+	livros = content.find_all("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
+	for livro in livros:
+		titulo = livro.find("h3").text
+		preco = livro.find("p", class_="price_color").text
+		estoque = livro.find("p", class_="instock availability").text.strip()
+		estoque = "disponivel" if "In stock" in estoque else "indisponivel"
+		print(f"titulo: {titulo} \n"+
+		f"preco: {preco}\n"+
+		f"estoque: {estoque}")
+		print("-"*10)
+else:
+	print("Nao conectado")
